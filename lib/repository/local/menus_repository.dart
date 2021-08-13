b00c3640cf20f851543000562dc9268c6e3fca43
b1017173 <g2121034@fun.ac.jp>
2021-08-13 18:06:31 +0900

    [add]add関数の再実装

--- lib/repository/local/menus_repository.dart
+++ lib/repository/local/menus_repository.dart
@@ -3,7 +3,6 @@ import 'package:hakondate_v2/model/foodstuff/foodstuff_model.dart';
 import 'package:hakondate_v2/model/menu/menu_model.dart';
 import 'package:hakondate_v2/model/nutrients/nutrients_model.dart';
 import 'package:hakondate_v2/model/quantity/quantity_model.dart';
-import 'package:hakondate_v2/model/school/school_model.dart';
 import 'package:hakondate_v2/repository/local/database_manager.dart';
 import 'package:moor/moor.dart';
 
@@ -14,82 +13,77 @@ class MenusRepository {
 
   late final DatabaseManager _databaseManager;
 
-  // Future<void> addMenu(MenuModel menu) async {
-  //   final int _id =
-  //       menu.day.year * 100000000 +
-  //       menu.day.month * 10000 +
-  //       menu.day.day * 100 +
-  //       menu.school.id;
-  //
-  //   final SchoolsTableCompanion _schoolSchema = SchoolsTableCompanion(
-  //     name: Value(menu.school.name),
-  //     lunchBlock: Value(menu.school.id),
-  //     classification: Value(menu.school.classification ?? 0)
-  //   );
-  //   final int _schoolId = await _repository.addSchoolsSchema(_schoolSchema);
-  //
-  //   final MenusTableCompanion _menuSchema = MenusTableCompanion(
-  //     id: Value(_id),
-  //     day: Value(menu.day),
-  //     schoolId: Value(_schoolId)
-  //   );
-  //   final int _menuId = await _repository.addMenusSchema(_menuSchema);
-  //
-  //   await Future.forEach(menu.dishes, (DishModel dish) async {
-  //     final int _dishId = await _addDish(dish);
-  //     await _repository.addMenuDishesSchema(
-  //       MenuDishesTableCompanion(
-  //         menuId: Value(_menuId),
-  //         dishId: Value(_dishId)
-  //       )
-  //     );
-  //   });
-  // }
-  //
-  // Future<int> _addDish(DishModel dish) async {
-  //   final DishesTableCompanion _dishSchema = DishesTableCompanion(
-  //     name: Value(dish.name),
-  //     category: Value(dish.category)
-  //   );
-  //   final int _dishId = await _repository.addDishesSchema(_dishSchema);
-  //   await Future.forEach(dish.foodstuffs, (FoodstuffModel foodstuff) async {
-  //     final int _foodstuffId = await _addFoodstuff(foodstuff);
-  //     await _repository.addDishFoodstuffsSchema(
-  //       DishFoodstuffsTableCompanion(
-  //         dishId: Value(_dishId),
-  //         foodstuffId: Value(_foodstuffId)
-  //       )
-  //     );
-  //   });
-  //
-  //   return _dishId;
-  // }
-  //
-  // Future<int> _addFoodstuff(FoodstuffModel foodstuff) async {
-  //   final FoodstuffsTableCompanion _foodstuffSchema = FoodstuffsTableCompanion(
-  //     name: Value(foodstuff.name),
-  //     piece: Value(foodstuff.quantity.piece),
-  //     gram: Value(foodstuff.quantity.gram),
-  //     energy: Value(foodstuff.nutrients.energy),
-  //     protein: Value(foodstuff.nutrients.protein),
-  //     lipid: Value(foodstuff.nutrients.lipid),
-  //     carbohydrate: Value(foodstuff.nutrients.carbohydrate),
-  //     sodium: Value(foodstuff.nutrients.sodium),
-  //     calcium: Value(foodstuff.nutrients.calcium),
-  //     magnesium: Value(foodstuff.nutrients.magnesium),
-  //     iron: Value(foodstuff.nutrients.iron),
-  //     zinc: Value(foodstuff.nutrients.zinc),
-  //     retinol: Value(foodstuff.nutrients.retinol),
-  //     vitaminB1: Value(foodstuff.nutrients.vitaminB1),
-  //     vitaminB2: Value(foodstuff.nutrients.vitaminB2),
-  //     vitaminC: Value(foodstuff.nutrients.vitaminC),
-  //     dietaryFiber: Value(foodstuff.nutrients.dietaryFiber),
-  //     salt: Value(foodstuff.nutrients.salt),
-  //     isHeat: Value(foodstuff.isHeat),
-  //     isAllergy: Value(foodstuff.isAllergy)
-  //   );
-  //   return await _repository.addFoodstuffsSchema(_foodstuffSchema);
-  // }
+  Future<int> addMenu(Map<String, dynamic> menu) async {
+    // TODO: 各スキーマの競合の解決処理を書く(仕様がよくわからん)
+    // KEYWORDS: UNIQUE制約 UPSERT
+    final MenusTableCompanion _menusSchema = MenusTableCompanion(
+      id: Value(menu['id']),
+      day: Value(menu['day']),
+      schoolId: Value(menu['schoolId']),
+      event: Value(menu['event'])
+    );
+    final int _menuId = await _databaseManager.addMenusSchema(_menusSchema);
+
+    await Future.forEach(menu['dishes'], (Map<String, dynamic> dish) async {
+      final int _dishId = await _addDish(dish);
+      await _databaseManager.addMenuDishesSchema(
+        MenuDishesTableCompanion(
+          menuId: Value(_menuId),
+          dishId: Value(_dishId)
+        )
+      );
+    });
+
+    return _menuId;
+  }
+
+  Future<int> _addDish(Map<String, dynamic> dish) async {
+    final DishesTableCompanion _dishSchema = DishesTableCompanion(
+      name: Value(dish['name']),
+      category: Value(dish['category'])
+    );
+    final int _dishId = await _databaseManager.addDishesSchema(_dishSchema);
+
+    await Future.forEach(dish['foodstuffs'], (Map<String, dynamic> foodstuff) async {
+      final int _foodstuffId = await _addFoodstuff(foodstuff);
+      await _databaseManager.addDishFoodstuffsSchema(
+        DishFoodstuffsTableCompanion(
+          dishId: Value(_dishId),
+          foodstuffId: Value(_foodstuffId)
+        )
+      );
+    });
+
+    return _dishId;
+  }
+
+  Future<int> _addFoodstuff(Map<String, dynamic> foodstuff) async {
+    return await _databaseManager.addFoodstuffsSchema(
+      FoodstuffsTableCompanion(
+        name: Value(foodstuff['name']),
+        piece: Value(foodstuff['piece']),
+        gram: Value(foodstuff['gram']),
+        energy: Value(foodstuff['energy']),
+        protein: Value(foodstuff['protein']),
+        lipid: Value(foodstuff['lipid']),
+        carbohydrate: Value(foodstuff['carbohydrate']),
+        sodium: Value(foodstuff['sodium']),
+        calcium: Value(foodstuff['calcium']),
+        magnesium: Value(foodstuff['magnesium']),
+        iron: Value(foodstuff['iron']),
+        zinc: Value(foodstuff['zinc']),
+        retinol: Value(foodstuff['retinol']),
+        vitaminB1: Value(foodstuff['vitaminB1']),
+        vitaminB2: Value(foodstuff['vitaminB2']),
+        vitaminC: Value(foodstuff['vitaminC']),
+        dietaryFiber: Value(foodstuff['dietaryFiber']),
+        salt: Value(foodstuff['salt']),
+        isHeat: Value(foodstuff['isHeat']),
+        isAllergy: Value(foodstuff['isAllergy']),
+        origin: Value(foodstuff['origin'])
+      )
+    );
+  }
 
   Future<List<MenuModel>> getAllMenus() async {
     List<MenuModel> _menus = [];
@@ -115,7 +109,6 @@ class MenusRepository {
 
   Future<MenuModel> getMenuById(int menuId) async {
     final MenusSchema _menusSchema = await _databaseManager.getMenusSchemaById(menuId);
-    // final SchoolsSchema _schoolsSchema = await _repository.getSchoolsSchemaById(_menusSchema.schoolId);
 
     return _getMenuBySchema(_menusSchema);
   }
@@ -181,7 +174,8 @@ class MenusRepository {
         salt: _foodstuffsSchema.salt
       ),
       isAllergy: _foodstuffsSchema.isAllergy,
-      isHeat: _foodstuffsSchema.isHeat
+      isHeat: _foodstuffsSchema.isHeat,
+      origin: _foodstuffsSchema.origin
     );
   }
 }
