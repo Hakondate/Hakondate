b764ca3b26848332963533206471c19df4af03f6
b1017173 <g2121034@fun.ac.jp>
2022-05-13 10:15:16 +0900

    [remove]使ってない関数の削除

--- lib/model/dish/dish_category.dart
+++ lib/model/dish/dish_category.dart
@@ -8,18 +8,3 @@ enum DishCategory {
   const DishCategory({required this.value});
   final String value;
 }
-
-extension DishModelExtension on DishCategory? {
-  String getValue() {
-    switch (this) {
-      case DishCategory.main:
-        return 'main';
-      case DishCategory.drink:
-        return 'drink';
-      case DishCategory.side:
-        return 'side';
-      default:
-        return 'side';
-    }
-  }
-}
