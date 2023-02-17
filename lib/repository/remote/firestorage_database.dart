e8f9fd2c0ab9892849f894c595a75b77164336e7
Micad <g2121034@fun.ac.jp>
2023-02-18 01:36:30 +0900

    [add]firebase_storage package

---
+++ lib/repository/remote/firestorage_database.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final firestorageDatabaseProvider = StateNotifierProvider<FirestorageDatabase, FirebaseStorage>((_) {
+  final FirebaseStorage storage = FirebaseStorage.instance;
+  return FirestorageDatabase(storage);
+});
+
+class FirestorageDatabase extends StateNotifier<FirebaseStorage> {
+  FirestorageDatabase(super.storage);
+
+  Reference get lettersReference => state.ref().child('letters');
+}
