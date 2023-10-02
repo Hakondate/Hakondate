5c01c5b16bc6ada8ef3d1c6c550f7eef9bfa2691
Atori <atori.ikeyama@gmail.com>
2023-10-02 16:38:50 +0900

    [fix] remove unused import

--- lib/view/daily/daily.dart
+++ lib/view/daily/daily.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hakondate/view_model/multi_page/user/user_view_model.dart';
-import 'package:hakondate/view_model/single_page/splash/splash_view_model.dart';
 import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 
