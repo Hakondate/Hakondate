3652f0e7f5f122f4c8f0730c0a04dee69018e405
Atori-Ikeyama <Atori-Ikeyama@users.noreply.github.com>
2023-12-27 12:17:58 +0000

    bot: run generator and sorter
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- lib/view/component/form/school_form.dart
+++ lib/view/component/form/school_form.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:hakondate/constant/size.dart';
 import 'package:hakondate/model/school/school_model.dart';
 import 'package:hakondate/state/signup/signup_state.dart';
