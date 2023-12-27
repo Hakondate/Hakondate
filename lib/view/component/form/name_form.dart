95453efc0a72e2ad4f5f789e7d4e9795b4e0f5a3
Atori <atori.ikeyama@gmail.com>
2023-12-27 21:39:50 +0900

    update: run import soter

--- lib/view/component/form/name_form.dart
+++ lib/view/component/form/name_form.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:hakondate/constant/app_color.dart';
 import 'package:hakondate/constant/size.dart';
 import 'package:hakondate/state/signup/signup_state.dart';
