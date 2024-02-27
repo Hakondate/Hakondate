53e7bf70e0045554e2cc87e06bca71b8e3a1fe4d
Idalgo <discoverywant30@gmail.com>
2024-02-28 00:47:59 +0900

    給食お休みや、献立準備中の日でもスワイプに対応させたが、これらの日のときの画像表示位置が上にずれ、下部分はスワイプに反応しなくなっていた。

--- lib/view/daily/daily.dart
+++ lib/view/daily/daily.dart
@@ -154,15 +154,57 @@ class Daily extends StatelessWidget {
                     ),
                   );
                 } else if (state.menu is HolidayMenuModel) {
-                  return const NonLunchesDayBody(
-                    imageFileName: 'holiday.png',
-                    text: '給食はお休みです...',
+                  return GestureDetector(
+                    onHorizontalDragEnd: (DragEndDetails details) {
+                      if (details.primaryVelocity! < 0) {
+                        ref
+                            .read(dailyViewModelProvider.notifier)
+                            .updateSelectedDay(
+                              selectedDay: ref
+                                  .read(dailyViewModelProvider.notifier)
+                                  .getAddedSelectedDay(state, 1),
+                            );
+                      } else {
+                        ref
+                            .read(dailyViewModelProvider.notifier)
+                            .updateSelectedDay(
+                              selectedDay: ref
+                                  .read(dailyViewModelProvider.notifier)
+                                  .getAddedSelectedDay(state, -1),
+                            );
+                      }
+                    },
+                    child: const NonLunchesDayBody(
+                      imageFileName: 'holiday.png',
+                      text: '給食はお休みです...',
+                    ),
                   );
                 }
 
-                return const NonLunchesDayBody(
-                  imageFileName: 'no_data.png',
-                  text: '献立は準備中です...',
+                return GestureDetector(
+                  onHorizontalDragEnd: (DragEndDetails details) {
+                    if (details.primaryVelocity! < 0) {
+                      ref
+                          .read(dailyViewModelProvider.notifier)
+                          .updateSelectedDay(
+                            selectedDay: ref
+                                .read(dailyViewModelProvider.notifier)
+                                .getAddedSelectedDay(state, 1),
+                          );
+                    } else {
+                      ref
+                          .read(dailyViewModelProvider.notifier)
+                          .updateSelectedDay(
+                            selectedDay: ref
+                                .read(dailyViewModelProvider.notifier)
+                                .getAddedSelectedDay(state, -1),
+                          );
+                    }
+                  },
+                  child: const NonLunchesDayBody(
+                    imageFileName: 'no_data.png',
+                    text: '献立は準備中です...',
+                  ),
                 );
               },
               orElse: () => const SizedBox.shrink(),
