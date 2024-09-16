71ec6d899643ad68665992cec02b250aaea24119
Atori-Ikeyama <Atori-Ikeyama@users.noreply.github.com>
2024-09-16 13:33:05 +0000

    bot: run generator and sorter
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- lib/view_model/single_page/daily/daily_view_model.dart
+++ lib/view_model/single_page/daily/daily_view_model.dart
@@ -1,13 +1,13 @@
-import 'package:hakondate/model/foodstuff/foodstuff_model.dart';
-import 'package:hakondate/view_model/multi_page/user/user_view_model.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import 'package:hakondate/model/dish/dish_model.dart';
+import 'package:hakondate/model/foodstuff/foodstuff_model.dart';
 import 'package:hakondate/model/menu/menu_model.dart';
 import 'package:hakondate/repository/local/sqlite/menus/menus_local_repository.dart';
 import 'package:hakondate/state/daily/daily_state.dart';
 import 'package:hakondate/util/analytics_controller/analytics_controller.dart';
 import 'package:hakondate/util/environment.dart';
+import 'package:hakondate/view_model/multi_page/user/user_view_model.dart';
 
 part 'daily_view_model.g.dart';
 
